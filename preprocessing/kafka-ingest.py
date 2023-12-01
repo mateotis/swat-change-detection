b7d52b52ef38ef679575dbbcfb763f78a98cad70
mateotis <hekfuszmate@gmail.com>
2023-12-01 17:06:44 +0100

    Successfully parsing and preprocessing Kafka stream in Spark.

--- preprocessing/kafka-ingest.py
+++ preprocessing/kafka-ingest.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from confluent_kafka import Producer
+import time
 
 def delivery_report(err, msg):
 	if err is not None:
@@ -9,15 +10,21 @@ df = pd.read_excel("../data/swat-dataset.xlsx", skiprows = 1) # Load SWaT data
 df = df.drop(0, axis = 0) # Remove unnecessary row
 df = df.reset_index()
 
+df = df[["GMT +0", "FIT 401", "LIT 301", "P601 Status", "MV201", "P101 Status", "MV 501", "P301 Status"]] # Reduce dataset to the columns that will be attacked (plus the timestamp)
+
 prod = Producer({'bootstrap.servers': 'localhost:9092'}) # Set up producer
 topic = "water-treatment"
 
 for idx, row in df.iterrows():
-	jsonRow = row.to_json() # Convert data to json
-	prod.produce(topic, jsonRow, callback=delivery_report) # Ingest into Kafka
+	timestamp = row["GMT +0"] # Pass the timestamp as the key
+	jsonValue = row.drop("GMT +0").to_json() # Convert data to json
+	prod.produce(topic, key = timestamp, value = jsonValue, callback=delivery_report) # Ingest into Kafka
 	prod.flush() # Ensure it's delivered
 	
 	print("\r", end = '')
 	print(idx, "records delivered.", end = '', flush = True)
+
+	if(idx % 100 == 0): # Sleep for a second after every 100 records to simulate the arrival of data in "real-time" 
+		time.sleep(1)
 
 print("\n")
--- preprocessing/spark-preproc.py
+++ preprocessing/spark-preproc.py
@@ -1,11 +1,24 @@
-import pyspark
-import time
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+# We know the structure of the data coming in from Kafka
+schema = StructType([
+    StructField("FIT 401", DoubleType()),
+    StructField("LIT 301", DoubleType()),
+    StructField("P601 Status", IntegerType()),
+    StructField("MV201", IntegerType()),
+    StructField("P101 Status", IntegerType()),
+    StructField("MV 501", IntegerType()),
+    StructField("P301 Status", IntegerType()),
+])
 
 spark = SparkSession.builder \
+    .appName("SWaT data preprocessing") \
     .master("local") \
 	.getOrCreate()
 
+# Consume the data from Kafka
 df = spark \
   .readStream \
   .format("kafka") \
@@ -15,10 +28,12 @@ df = spark \
   .load() # startingOffsets must be set to earliest so we can get all the data from the Kafka broker
 
 df = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+df = df.withColumn("data", from_json(df.value, schema)) # Deserialise the actual data from the json string and add it as a new column
+df = df.select(col("key").alias("timestamp"), "data.*") # Get the timestamp (the key) and the features in their separate columns in one dataframe
+
+df.printSchema()
 
 query = df.writeStream.format("console").start() # Print stream to console as it arrives
 query.awaitTermination() # Don't continue until the data is printed
 
 
-#print(df.first())
-#print(df.describe())
