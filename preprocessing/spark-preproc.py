4fcb337b70477b3847340c2a3ee281a56b8ef609
mateotis <hekfuszmate@gmail.com>
2023-12-12 13:24:48 +0100

    Data is now imported into Influx with the original timestamps. Login details and tokens are now stored in an .env file. I've also updated the readme to cover all the steps to get the entire pipeline working.

--- preprocessing/spark-preproc.py
+++ preprocessing/spark-preproc.py
@@ -31,6 +31,8 @@ spark = SparkSession.builder \
     .master("local") \
 	.getOrCreate()
 
+spark.sparkContext.setLogLevel("WARN") # Reduce Spark's logging verbosity so we can actually see our own output
+
 # Consume the data from Kafka
 df = spark \
   .readStream \
@@ -44,7 +46,7 @@ df = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
 df = df.withColumn("data", from_json(df.value, schema)) # Deserialise the actual data from the json string and add it as a new column
 df = df.select(col("key").alias("timestamp"), "data.*") # Get the timestamp (the key) and the features in their separate columns in one dataframe
 
-df = df.withColumn("timestamp", col("timestamp").cast("timestamp")) # Convert timestamp into actual timestamp format
+df = df.withColumn("timestamp", to_timestamp(date_format(col("timestamp"), "yyyy-MM-dd HH:mm:ss"))) # Convert timestamp into actual readable timestamp format and cut off the milliseconds
 
 df = attackLabeling(df) # Add attack/normal labeling
 
@@ -52,7 +54,6 @@ df.printSchema()
 
 # Define a list of numerical column names
 numerical_columns = ["FIT 401", "LIT 301", "P601 Status", "MV201", "P101 Status", "MV 501", "P301 Status"]
-
 
 # Add streaming aggregations for count, mean, min, max, stddev, and rate of each numerical column
 agg_expr = []
@@ -77,13 +78,17 @@ agg_query.writeStream \
     .format("console") \
     .start()
 
-
-# TODO more preprocessing steps
-
 # Once all the preproc is done, feed the new data back into Kafka in the same key-value format but under a different topic
 topic = "water-treatment-preproc"
+procColumns = [col("timestamp").cast("String").alias("timestamp")] # Convert the timestamp back to string format as the JSON conversion will automatically change its readable format (which we don't want)
+for column in df.columns[1:]: # The rest of the (numerical) columns are untouched
+    procColumns.append(col(column))
+
 # We're basically reversing the steps we did when we consumed the data: renaming timestamp back to be the key and turning the features back into a json string to serve as the value
-procDf = df.select(col("timestamp").alias("key"), to_json(struct(df.columns[1:])).alias("value"))
+procDf = df.select(col("timestamp").alias("key"), to_json(struct(procColumns)).alias("value"))
+'''Note that the timestamp is included in the value json too.
+We do this so we can specify in the Telegraf config that it should use that column as the timestamp when putting the data into InfluxDB. 
+'''
 procDf.printSchema()
 
 ds = procDf \
@@ -96,6 +101,7 @@ ds = procDf \
   .start()
 ds.awaitTermination()
 
-#ds = df.writeStream.format("console").start() # Print stream to console as it arrives
-
+'''ds1 = df.writeStream.format("console").option("truncate", False).start()
+ds2 = procDf.writeStream.format("console").option("truncate", False).start() # Print stream to console as it arrives
+ds2.awaitTermination()'''
 
